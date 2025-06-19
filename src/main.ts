3489c06ac326a0898b5b6d87436c1e8a44b3f230
Avinash <avinash@weare86.com>
2025-06-19 11:11:28 +0530

    basic improvements on config

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe, Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const logger = new Logger('');
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
   app.useGlobalPipes(
     new ValidationPipe({ forbidNonWhitelisted: true, whitelist: true }),
   );
-  const port = process.env.PORT ?? 3000;
+  const port = configService.get<number>('PORT') ?? 3000;
   await app.listen(port);
   logger.log(`Application is running on port: ${port}`);
 }
